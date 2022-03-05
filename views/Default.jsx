08afcd0dd78025319926a10d46f800c6d29d8df5
buttonpusher <anthonymrondina@gmail.com>
2022-03-04 23:08:36 -0500

    minor changes to Default.jsx file

--- views/Default.jsx
+++ views/Default.jsx
@@ -78,64 +78,66 @@ class DefaultLayout extends React.Component {
                             {this.props.children}
                         </main>
                         <footer class="footer">
-                            <div class="row1">
-                                <div class="row1Links">
-                                    <span >Get to know us</span>
-                                    <ul>
-                                        <li>Careers</li>
-                                        <li>Blog</li>
-                                        <li>About Amazon</li>
-                                        <li>Sustainability</li>
-                                        <li>Press Center</li>
-                                        <li>Investor Relations</li>
-                                        <li>Amazon Devices</li>
-                                        <li>Amazon Science</li>
-                                    </ul>
+                            <div className="rowDiv">
+                                <div class="row1">
+                                    <div class="row1Links">
+                                        <span >Get to know us</span>
+                                        <ul>
+                                            <li>Careers</li>
+                                            <li>Blog</li>
+                                            <li>About Amazon</li>
+                                            <li>Sustainability</li>
+                                            <li>Press Center</li>
+                                            <li>Investor Relations</li>
+                                            <li>Amazon Devices</li>
+                                            <li>Amazon Science</li>
+                                        </ul>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="row2">
-                                <div class="row2Links">
-                                    <span >Make money with us</span>
-                                    <ul>
-                                        <li>Careers</li>
-                                        <li>Blog</li>
-                                        <li>About Amazon</li>
-                                        <li>Sustainability</li>
-                                        <li>Press Center</li>
-                                        <li>Investor Relations</li>
-                                        <li>Amazon Devices</li>
-                                        <li>Amazon Science</li>
-                                    </ul>
+                                <div class="row2">
+                                    <div class="row2Links">
+                                        <span >Make money with us</span>
+                                        <ul>
+                                            <li>Careers</li>
+                                            <li>Blog</li>
+                                            <li>About Amazon</li>
+                                            <li>Sustainability</li>
+                                            <li>Press Center</li>
+                                            <li>Investor Relations</li>
+                                            <li>Amazon Devices</li>
+                                            <li>Amazon Science</li>
+                                        </ul>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="row3">
-                                <div class="row3Links">
-                                    <span >Amazon Payment Products</span>
-                                    <ul>
-                                        <li>Careers</li>
-                                        <li>Blog</li>
-                                        <li>About Amazon</li>
-                                        <li>Sustainability</li>
-                                        <li>Press Center</li>
-                                        <li>Investor Relations</li>
-                                        <li>Amazon Devices</li>
-                                        <li>Amazon Science</li>
-                                    </ul>
+                                <div class="row3">
+                                    <div class="row3Links">
+                                        <span >Amazon Payment Products</span>
+                                        <ul>
+                                            <li>Careers</li>
+                                            <li>Blog</li>
+                                            <li>About Amazon</li>
+                                            <li>Sustainability</li>
+                                            <li>Press Center</li>
+                                            <li>Investor Relations</li>
+                                            <li>Amazon Devices</li>
+                                            <li>Amazon Science</li>
+                                        </ul>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="row4">
-                                <div class="row4Links">
-                                    <span >Let us help you</span>
-                                    <ul>
-                                        <li>Careers</li>
-                                        <li>Blog</li>
-                                        <li>About Amazon</li>
-                                        <li>Sustainability</li>
-                                        <li>Press Center</li>
-                                        <li>Investor Relations</li>
-                                        <li>Amazon Devices</li>
-                                        <li>Amazon Science</li>
-                                    </ul>
+                                <div class="row4">
+                                    <div class="row4Links">
+                                        <span >Let us help you</span>
+                                        <ul>
+                                            <li>Careers</li>
+                                            <li>Blog</li>
+                                            <li>About Amazon</li>
+                                            <li>Sustainability</li>
+                                            <li>Press Center</li>
+                                            <li>Investor Relations</li>
+                                            <li>Amazon Devices</li>
+                                            <li>Amazon Science</li>
+                                        </ul>
+                                    </div>
                                 </div>
                             </div>
                         </footer>
